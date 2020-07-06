db5525891bc1b63a4452ce0b2ab20bd598c9d8ba
Varun Ramani <varun.ramani@gmail.com>
2020-07-06 17:51:01 -0400

    Finished uninstaller

--- uninstaller.zsh
+++ uninstaller.zsh
@@ -1,11 +1,13 @@
 echo "\033[1;31mZConf Uninstaller :(\033[0m"
 echo "\033[1;31m--------------------\033[0m"
 
-printf "\033[1;33mThe following will be removed:\n\t$HOME/.zconf/\n\t$HOME/bin/zconf\n\t$HOME/.zshrc? (y/n) \033[0m"
+printf "\033[1;33mThe following will be removed:\n\t$HOME/.zconf/\n\t$HOME/bin/zconf\nContinue? (y/n) \033[0m"
 if ! read -q; then
     exit
 fi
 
 echo
-echo "Now deleting $HOME/.zconf/, $HOME/bin/zconf, and $HOME/.zshrc"
-rm -rf $HOME/.zconf $HOME/bin/zconf
+echo "Deleting $HOME/.zconf/"
+rm -rf $HOME/.zconf
+echo "Deleting $HOME/bin/zconf"
+rm -rf $HOME/bin/zconf
