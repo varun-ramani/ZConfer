d628df07cd6b6700656f8ce88d3975277cb1bc66
Varun Ramani <varun.ramani@gmail.com>
2020-07-06 23:20:29 -0400

    Added globals for path and alias

--- globals.py
+++ globals.py
@@ -5,3 +5,11 @@ zconf_home = home + "/.zconf"
 zshrc = home + "/.zshrc"
 zconf_main = zconf_home + "/managed_zshrc"
 zshrc_backup = home + "/.zshrc.bak"
+
+class jsondata:
+    path = zconf_home + "/path.json"
+    aliases = zconf_home + "/alises.json"
+
+class modules:
+    path = zconf_home + "/path.zsh"
+    aliases = zconf_home + "/aliases.zsh"
