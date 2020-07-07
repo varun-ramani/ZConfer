8c14dc21434bc8116df98fbf398bbcba1aa2a44e
Varun Ramani <varun.ramani@gmail.com>
2020-07-06 23:20:17 -0400

    Finished init up to path

--- init.py
+++ init.py
@@ -1,6 +1,8 @@
 from utils import colorprint, confirm, write_file
 import os
 import globals
+import json
+import path
 
 boiler_zshrc = """
 # Load zconf 
@@ -12,6 +14,7 @@ source {}
 
 boiler_zconf_main = """
 
+# ==> DON'T EDIT THIS FILE BY HAND <==
 source {0}/path.zsh
 source {0}/alias.zsh
 
@@ -35,5 +38,24 @@ def initialize():
         if not confirm(colorprint("\nDestroy your existing configuration and start fresh? (y/n) ", "yellow")):
             return
 
+    print("Creating core config file at {}".format(globals.zconf_main))
     write_file(globals.zconf_main, boiler_zconf_main)
 
+    print("JSON data files:")
+    print("\tCreating {}".format(globals.jsondata.path))
+    write_file(
+        globals.jsondata.path, 
+        json.dumps({}, indent=4)
+    )
+    print("\tCreating {}".format(globals.jsondata.aliases))
+    write_file(
+        globals.jsondata.aliases, 
+        json.dumps({
+            "gclone": "git clone",
+            "gpush": "git push",
+            "gpull": "git pull"
+        }, indent=4)
+    )
+    print("Module files:")
+    print("\tGenerating {} from {}".format(globals.modules.path, globals.jsondata.path))
+    path.generate()
